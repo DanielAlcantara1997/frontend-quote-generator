a2aa8a3b9241f975130d7a936bba468183992485
DanielAlcantara1997 <gen.danielalcantara@gmail.com>
2024-05-06 10:48:46 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function App() {
+  const [quote, setQuote] = useState({from: "", text: "Generate Quote"})
+  const [btnText, setBtnText] = useState("Generate Quote")
+
+  const generateQuote = async () => {
+    try {
+      setBtnText("Wait")
+      const response = await fetch(`https://quote-api-t039.onrender.com/api/randomize`)
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json()
+      setBtnText("Generate Quote")
+      setQuote(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row d-flex justify-content-center align-items-center vh-100">
+        <Card style={{ width: '18rem' }} className='text-center'>
+          <Card.Body>
+            <Card.Title>{quote.from}</Card.Title>
+            <Card.Text>
+              {quote.text}
+            </Card.Text>
+            <Button variant="primary" onClick={() => generateQuote()}>{btnText}</Button>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
